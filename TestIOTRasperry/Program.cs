83cdee0f5d0b03829245e82e3be842b199614696
lluis cantón callau <lluis.cantoncallau@altran.com>
2020-11-03 17:23:03 +0100

    InitialCommit

---
+++ TestIOTRasperry/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Device.Gpio;
+using System.Threading;
+
+namespace TestIOTRasperry
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Start program");
+            var pin = 4;
+            var controller = new GpioController();
+            controller.OpenPin(pin,PinMode.Output );
+
+            while (true)
+            {
+                controller.Write(pin, PinValue.High);
+                Console.Write("PIN: " + pin.ToString() + " High");
+                Thread.Sleep(1000);
+                controller.Write(pin, PinValue.Low);
+                Console.Write("PIN: " + pin.ToString() + " Low");
+                Thread.Sleep(1000);
+            }
+           
+        }
+    }
+}
